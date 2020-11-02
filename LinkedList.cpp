f947e10f8403a0f346c777a7887ffdf5f46b891b
ca3hh <carter.angelus@richmond.edu>
2020-11-02 17:07:32 -0500

    added get method

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,6 +1,7 @@
 #include "LinkedList.h"
 #include <vector>
 #include <list>
+#include <iostream>
 
 
 // constructor template class
@@ -19,12 +20,24 @@ template <class T>
 int LinkedList<T>::size() const {
 	typename std::list<T>::iterator it = myList.begin();
 	int tempCounter{0};
-	while (it.next != NULL) {
+	while (it.next() != NULL) {
 		tempCounter += 1;
 	}
 	
 	return{tempCounter};
 }
 
+template <class T>
+T LinkedList<T>::get(int index) const {
+	typename std::list<T>::iterator it = myList.begin();
+	if (index > myList.size()){
+		std::cerr << "invalid argument" << endl;
+	}
+
+	for (int i = 0; i < index; i++) {
+		it.next();
+	}
+	return(*it);
+}
 
 
