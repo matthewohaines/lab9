0d8e65c39ebf676e1e541e690324ae8860e2b2bb
Matthew Haines <matthewohaines@gmail.com>
2020-11-04 12:22:34 -0500

    fixed toarray method

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,4 +1,5 @@
 #include "LinkedList.h"
+#include <iostream>
 
 using namespace std;
 
@@ -13,7 +14,7 @@ LinkedList<T>::LinkedList()
 template <class T>
 LinkedList<T>::LinkedList(LinkedList& list) 
 {
-	// new code needed 
+	std::list<T> myList = list;
 }
 
 template <class T> 
@@ -28,9 +29,9 @@ void LinkedList<T>::add(T element)
 template <class T>
 int LinkedList<T>::size() const 
 {
-	typename std::list<T>::iterator it = myList.begin();
+	typename std::list<T>::const_iterator it = myList.begin();
 	int tempCounter{0};
-	while (it.next() != NULL) {
+	while (it++ != myList.end()) {
 		tempCounter += 1;
 	}
 	
@@ -40,13 +41,13 @@ int LinkedList<T>::size() const
 template <class T>
 T LinkedList<T>::get(int index) const 
 {
-	typename std::list<T>::iterator it = myList.begin();
-	if (index > myList.size() || index < 0 || myList.size() == 0){
-		throw std::invalid_argument("invalid argument");
+	typename std::list<T>::const_iterator it = myList.begin();
+	if (index > (int)myList.size() || index < 0 || (int)myList.size() == 0){
+		throw std::invalid_argument("invalid index: ");
 	}
 
 	for (int i = 0; i < index; i++) {
-		it.next();
+		it++;
 	}
 	return(*it);
 }
@@ -55,12 +56,12 @@ template <class T>
 T LinkedList<T>::remove(int index) 
 {
 	typename std::list<T>::iterator it = myList.begin();
-	if (index > myList.size() || index < 0 || myList.size() == 0){
-		throw std::invalid_argument("invalid argume");
+	if (index > (int)myList.size() || index < 0 || (int)myList.size() == 0){
+		throw std::invalid_argument("invalid argumet");
 	}
 
 	for (int i = 0; i < index; i++) {
-		it.next();
+		it++;
 	}
 	T element = *it;
 	myList.erase(it);
@@ -70,21 +71,24 @@ T LinkedList<T>::remove(int index)
 template <class T>
 std::vector<T> LinkedList<T>::toArray() const
 {
-	typename std::list<T>::iterator it  = myList.begin();
 	std::vector<T> vec;
-	for (int i = 0; i < myList.size(); ++i)
+	for (T const &c: myList)
 	{
-		T element = *it;
-		vec.push_back(element);
+		vec.push_back(c);
 	}
 	return vec;
 }
 
 template <class T>
-LinkedList<T>& operator+=(const T& item){
-	myList.merge(item);
-	return myList;
+LinkedList<T>& LinkedList<T>::operator+=(const T& item){
+	myList.push_back(item);
+	return(*this);	
 }
+
+
+
+
+
 
 
 
--- tester.cpp
+++ tester.cpp
@@ -1,20 +1,102 @@
 #include <string>
 #include <iostream>
 #include "LinkedList.h"
+#include "LinkedList.cpp"
 
 using namespace std;
 
-int main(){
+int main()
+{
 
 	cout << "Testing Constructor" << endl;
-	LinkedList<int> linkedlist1; 
+	LinkedList<int> linkedlist1;
+	cout << "variable initialized" << endl; 
 
 	cout << "Adding ints to linkedlist1" << endl;
 	linkedlist1.add(1);
+	cout << "Adding 1: should print 1 from get(0): " << linkedlist1.get(0) << endl;
 	linkedlist1.add(3);
+	cout << "Adding 3: should print 3 from get(1): " << linkedlist1.get(1) << endl;
 	linkedlist1.add(8);
-	cout << "linked list: " << linkedlist1.toArray() << endl;
+	cout << "Adding 8: should print 8 from get(2): " << linkedlist1.get(2) << endl;
 
+	cout << "-----------------------------------" << endl;
+
+	cout << "testing error throws in get()" << endl;
+	try{
+		linkedlist1.get(8);
+	}
+	catch (const invalid_argument& e) {
+		printf("trying to access element 8 gives message: %s", e.what());
+		cout << 8 << " size is : " << linkedlist1.size() << endl;
+	}
+
+	try{
+                linkedlist1.get(-1);
+        }
+        catch (const invalid_argument& e) {
+                printf("trying to access element -1 gives message: %s", e.what());
+                cout << -1 << " size is : " << linkedlist1.size() << endl;
+        }
+
+	cout << "-----------------------------------" << endl;
+
+	cout << "testing remove function" << endl;
+
+	cout << "size: " << linkedlist1.size() << endl;
+	linkedlist1.remove(0);
+	cout << "removing first element/head" << endl;
+	cout << "size: " << linkedlist1.size() << endl;
+
+	cout << "-----------------------------------" << endl;
+
+	cout << "size: " << linkedlist1.size() << endl;
+        linkedlist1.remove(1);
+        cout << "removing last element/tail" << endl;
+        cout << "size: " << linkedlist1.size() << endl;
+
+	cout << "-----------------------------------" << endl;
+
+	cout << "clearing list and attempting ot access empty list" << endl;
+
+	cout << "removing last element" << endl;
+	linkedlist1.remove(0);
+	cout << "size: " << linkedlist1.size() << endl; 
+
+	try{
+                linkedlist1.get(8);
+        }
+        catch (const invalid_argument& e) {
+                printf("trying to access empty list gives message: %s", e.what());
+                cout << 0 << " size is : " << linkedlist1.size() << endl;
+	}
+
+	cout << "-----------------------------------" << endl;
+
+	cout << "testing operator overload" << endl;
+	
+	cout << "adding 1" << endl;
+	linkedlist1+=3;
+	cout << linkedlist1.size() << endl;
+
+
+	cout << "-----------------------------------" << endl;
+
+	cout << "testing toArray method" << endl;
+	cout << "making new linklist of chars" << endl;
+	LinkedList<char> linkedlistchar;
+	cout << "adding chars to linked list of chars" << endl;
+	linkedlistchar.add('a');
+	linkedlistchar.add('b');
+	linkedlistchar.add('c');
+	cout << "Expected toArray output: a b c" << endl;
+	cout << "Observed toArray output: " << endl;
+	vector<char> v = linkedlistchar.toArray();
+	for (int i = 0; i < v.size(); ++i)
+	{
+		cout << v[i] << endl;
+	}
 
 
 	return 0;
+}
