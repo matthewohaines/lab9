e59c3587e202749da758155d27b5b28aa886d63b
ca3hh <carter.angelus@richmond.edu>
2020-11-03 21:24:28 -0500

    fixed linking error

--- tester.cpp
+++ tester.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include "LinkedList.h"
+#include "LinkedList.cpp"
 
 using namespace std;
 
@@ -12,11 +13,12 @@ int main()
 
 	cout << "Adding ints to linkedlist1" << endl;
 	linkedlist1.add(1);
+	cout << "Adding 1" << endl;
 	linkedlist1.add(3);
+	cout << "Adding 3" << endl;
 	linkedlist1.add(8);
+	cout << "Adding 8" << endl;
 	//cout << "linked list: " << linkedlist1.toArray() << endl;
-
-
 
 	return 0;
 }
