ef3bcfa6ec16bfd76297a49d97e1fd323fd253ed
ca3hh <carter.angelus@richmond.edu>
2020-11-03 20:38:51 -0500

    commented out vector section

--- tester.cpp
+++ tester.cpp
@@ -14,7 +14,7 @@ int main()
 	linkedlist1.add(1);
 	linkedlist1.add(3);
 	linkedlist1.add(8);
-	cout << "linked list: " << linkedlist1.toArray() << endl;
+	//cout << "linked list: " << linkedlist1.toArray() << endl;
 
 
 
